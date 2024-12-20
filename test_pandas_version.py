99a702aaf1d27b560a6a8841bf7b803e81bf530e
Debugging-Tech <lightweightcobra@gmail.com>
2024-12-20 10:14:20 +0530

    Update test_pandas_version.py

--- test_pandas_version.py
+++ test_pandas_version.py
@@ -3,8 +3,7 @@ PANDAS_VERSION = "1.5.3"
 
 def test_pandas_version():
     ''' Use an assertion to check the output of pd.__version__ '''
-    assert pd.__version__ in [PANDAS_VERSION]
-
+    assert pd.__version__ == PANDAS_VERSION, f"Expected version {PANDAS_VERSION}, but got {pd.__version__}"
 
 if __name__ == "__main__":
     test_pandas_version()
